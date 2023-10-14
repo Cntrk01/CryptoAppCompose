2c1e40932c3828443caac0ef3801160957f3141b
Cntrk01 <karabulut_3828@hotmail.com>
2023-10-14 21:22:42 +0300

    Navigation Compose
    Burada navigation compose nedir nasıl yapılır onları işledik

--- app/src/main/java/com/compose/cryptoappcompose/MainActivity.kt
+++ app/src/main/java/com/compose/cryptoappcompose/MainActivity.kt
@@ -3,44 +3,56 @@ package com.compose.cryptoappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.compose.cryptoappcompose.ui.theme.CryptoAppCompose
+import com.compose.cryptoappcompose.view.CryptoDetailScreen
+import com.compose.cryptoappcompose.view.CryptoListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppCompose {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                //değişkenlerden birisinde değişiklik yada composable da oldugunda yani detaydan geri geldiğimizde yeni verilerin gelmesi durumunda
+                //recomposition işlemi olmaması için.Yani bütün ekranı değilde değişiklik olan fonksiyon değişkenlerin tekrar oluşmasını sağlıyor.Remember!
+                //rememberNavController() NavController'ın yeniden oluşturulmasına neden olacaktır.
+                val navController= rememberNavController()
+                //startDestination içinde bulunan stringi kafama göre yazdım ve aşağıda verdiğim ilkcomposable benim startım oluyo
+                NavHost(navController = navController, startDestination ="crypto_list_screen"){
+                    composable("crypto_list_screen"){
+                        //CryptoListScreen
+                        //Sonradan oluşturdugum classları buraya tanımlayabiliyorum
+                        //İlk çalıştıığı anda da bu ekranı görebiliyoruz.
+                        CryptoListScreen(navController = navController)
+                    }
+                    //Kripto detay sayfasına biz argümanla parametre alacağını söyledik. /{cryptoId}/{cryptoPrice} bunları buraya ekleyerek verilerin böyle gönderileceğini söyledik.
+                    composable("crypto_detail_string/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId"){
+                            type= NavType.StringType
+                        },
+                        navArgument("cryptoPrice"){
+                            type=NavType.StringType
+                        }
+                    )){
+                        //composable içinde verdiğimiz değişkenlere gelen değerleri artık burda listeleyebildik.
+                        val cryptoId= remember {
+                            it.arguments?.getString("cryptoId")
+                        }
+                        val cryptoPrice=remember {
+                            it.arguments?.getString("cryptoPrice")
+                        }
+                        CryptoDetailScreen(
+                            id = cryptoId ?: "",
+                            price = cryptoPrice ?: "",
+                            navController = navController)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoAppCompose {
-        Greeting("Android12")
     }
 }
