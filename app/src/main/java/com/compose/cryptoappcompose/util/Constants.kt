79dc0c90ca5c0457fd796fcd1c720f0f5963a035
Cntrk01 <karabulut_3828@hotmail.com>
2023-10-14 20:48:11 +0300

    First Commit To Main Branch

---
+++ app/src/main/java/com/compose/cryptoappcompose/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.compose.cryptoappcompose.util
+
+object Constants {
+    //d59d522d17ebe3909433308bab99af5278c66a6a
+    // currencies?key=<Kendi API Anahtarınız>&ids=BTC&attributes=id,name,logo_url
+    const val BASE_URL="https://api.nomics.com/v1/"
+    const val API_KEY="d59d522d17ebe3909433308bab99af5278c66a6a"
+    const val CALL_ATTRIBUTES="id,name,logo_url"
+}
