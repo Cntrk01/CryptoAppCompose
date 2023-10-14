c393bff64d36d7c3db98005ef8555453351e2e2b
Cntrk01 <karabulut_3828@hotmail.com>
2023-10-14 21:25:09 +0300

    Update Development
    Yorum satırlarıını kaldırdım

--- app/src/main/java/com/compose/cryptoappcompose/MainActivity.kt
+++ app/src/main/java/com/compose/cryptoappcompose/MainActivity.kt
@@ -18,19 +18,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppCompose {
-                //değişkenlerden birisinde değişiklik yada composable da oldugunda yani detaydan geri geldiğimizde yeni verilerin gelmesi durumunda
-                //recomposition işlemi olmaması için.Yani bütün ekranı değilde değişiklik olan fonksiyon değişkenlerin tekrar oluşmasını sağlıyor.Remember!
-                //rememberNavController() NavController'ın yeniden oluşturulmasına neden olacaktır.
                 val navController= rememberNavController()
-                //startDestination içinde bulunan stringi kafama göre yazdım ve aşağıda verdiğim ilkcomposable benim startım oluyo
                 NavHost(navController = navController, startDestination ="crypto_list_screen"){
                     composable("crypto_list_screen"){
-                        //CryptoListScreen
-                        //Sonradan oluşturdugum classları buraya tanımlayabiliyorum
-                        //İlk çalıştıığı anda da bu ekranı görebiliyoruz.
                         CryptoListScreen(navController = navController)
                     }
-                    //Kripto detay sayfasına biz argümanla parametre alacağını söyledik. /{cryptoId}/{cryptoPrice} bunları buraya ekleyerek verilerin böyle gönderileceğini söyledik.
                     composable("crypto_detail_string/{cryptoId}/{cryptoPrice}", arguments = listOf(
                         navArgument("cryptoId"){
                             type= NavType.StringType
@@ -39,7 +31,6 @@ class MainActivity : ComponentActivity() {
                             type=NavType.StringType
                         }
                     )){
-                        //composable içinde verdiğimiz değişkenlere gelen değerleri artık burda listeleyebildik.
                         val cryptoId= remember {
                             it.arguments?.getString("cryptoId")
                         }
