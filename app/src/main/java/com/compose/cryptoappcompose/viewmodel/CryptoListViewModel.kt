b44db82e007fb85d24261937cee48aa549463cca
Cntrk01 <karabulut_3828@hotmail.com>
2023-10-15 13:46:34 +0300

    Update

--- app/src/main/java/com/compose/cryptoappcompose/viewmodel/CryptoListViewModel.kt
+++ app/src/main/java/com/compose/cryptoappcompose/viewmodel/CryptoListViewModel.kt
@@ -12,18 +12,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CryptoListViewModel @Inject constructor(private val repository:CryptoRepository) : ViewModel() {
-    //<List<CryptoListItem>> bu veri tipi oldugunu belirtmezsek hata veriyor T generic veremezsin diye
+
     var cryptoList = mutableStateOf<List<CryptoListItem>>(listOf())
     var errorMessage= mutableStateOf("")
     var isLoading= mutableStateOf(false)
 
-    //viewmodelscope ile burada tanımlama yapabiliriz.Yada bu fonksiyonuda suspend yapabiliriz.Fakat bunların composda birer dezavantajları var henüz bahsetmedik.
     fun loadCrypto()=viewModelScope.launch{
         isLoading.value=true
         val result=repository.getCryptoList()
         when(result){
             is Resource.Success->{
-                //mapIndexed foreach mantıgındadır içindeki elemanları tek tek almamızı saglar
+
                 val cryptoItem=result.data!!.mapIndexed { index, cryptoListItem ->
                     CryptoListItem(cryptoListItem.currency,cryptoListItem.price)
                 } as List<CryptoListItem>
@@ -36,7 +35,6 @@ class CryptoListViewModel @Inject constructor(private val repository:CryptoRepos
                 errorMessage.value=result.message!!
                 isLoading.value=false
             }
-
             else -> {}
         }
     }
